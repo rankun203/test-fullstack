53a2ea8b6a2c4936e195262540a8d36165d8efdb
Kun Ran <rankun203@gmail.com>
2019-11-28 14:13:07 +0800

    fix: StoreProvider performance issue, resolving review in #35

--- src/store/store-provider.jsx
+++ src/store/store-provider.jsx
@@ -6,11 +6,11 @@ export const Store = React.createContext();
 
 export const StoreProvider = ({ children }) => {
   const [state, dispatch] = React.useReducer(reducer, initialState);
-  const value = { state, dispatch };
+  const value = React.useMemo(() => ({ state, dispatch }), [state, dispatch]);
 
   // For debug purpose only
   if (process.env.NODE_ENV === 'development') {
-    window.__app_store = state;
+    window.__app_reducer = value;
   }
 
   return <Store.Provider value={value}>{children}</Store.Provider>;
